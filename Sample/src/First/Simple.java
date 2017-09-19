29b4730fd65915730c82aa35ede814f2ef761ed8
manish.bontagarla <manish.bontagarla@Testing-21.infotreesolutions.com>
2017-09-19 12:57:27 +0530

    First Suite

---
+++ Sample/src/First/Simple.java
@@ -0,0 +1,13 @@
+package First;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Value of C is"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
